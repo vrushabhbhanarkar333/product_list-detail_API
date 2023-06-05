6d85f9e944c867efd8c793fdd597e13149885f58
surazkarn <surazkarn8848@gmail.com>
2023-06-05 17:37:23 +0530

    middleware and routes defined

--- index.js
+++ index.js
@@ -3,6 +3,13 @@ const express = require('express')
 const app = express()
 const PORT = process.env.PORT || 3001;
 
+// Middleware to parse JSON data
+app.use(express.json());
+
+// Routes
+const productRoutes = require('./src/routes/productRoutes');
+app.use('/api/products', productRoutes);
+
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
